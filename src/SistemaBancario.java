459b628175ca1d26717212ea7046eea8a87799e8
Juaino <jplay8827@gmail.com>
2024-05-28 19:34:56 -0300

    first commit

---
+++ src/SistemaBancario.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SistemaBancario {
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu Nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobreNome = scanner.next();
+
+        System.out.println("Digite sua Agência: ");
+        String agencia = scanner.next();
+        
+        System.out.println("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite seu saldo");
+        double saldo =scanner.nextDouble();
+
+        System.out.println("Olá " +nome+" "+sobreNome+" Obrigado por criar uma conta em nosso banco. Sua Agência é: " +agencia+". Conta: " +conta+ "E seu saldo de: " +saldo+ "Já está disponivel para saque!");
+
+    }
+    
+}
